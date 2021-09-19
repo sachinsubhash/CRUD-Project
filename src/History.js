2595d87a129d66b56be2ecace79a3da91587b69f
Sachin Subhash <sachin.sbsh2@gmail.com>
2021-09-19 18:17:52 +0400

    Update History.js

--- src/History.js
+++ src/History.js
@@ -1,3 +1,44 @@
 import { createBrowserHistory } from 'history';
 
 export default  createBrowserHistory();
+
+{*/import React from "react";
+import { Link } from "react-router-dom";
+import ContactListNav from "./ContactListNav";
+import {
+  Button,
+  Dialog,
+  DialogAcions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Select,
+} from "@mui/material";
+import { Input, Navbar, NavbarBrand, Label } from "reactstrap";
+import { AccountCircle, Menu } from "@material-ui/icons";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+function Update() {
+  return (
+    <div>
+      <h1>Personal Information</h1>
+      <div style={{display:'flex'}}>
+        <Box>
+          <TextField label="First Name" textarea="example" />
+          <TextField label="Last Name" textarea="example" />
+        </Box>
+      </div>
+      <div>
+        <Label> Email </Label>
+        <Box>
+          <TextField label='Email'/>
+        </Box>
+        <TextField />
+      </div>
+    </div>
+  );
+}
+
+export default Update;
+/*}
